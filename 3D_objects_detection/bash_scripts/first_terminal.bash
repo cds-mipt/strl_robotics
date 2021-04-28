62b69e1ae2360f84075aea8de978e214491d94fa
linukc <35877675+linukc@users.noreply.github.com>
2021-04-28 18:09:30 +0300

    Update first_terminal.bash

--- 3D_objects_detection/bash_scripts/first_terminal.bash
+++ 3D_objects_detection/bash_scripts/first_terminal.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #файл должен находится в Repos/solov2_pytorch1.7/bash_scripts/ на роботе
-./../docker/latest/start_devel.sh
-cd ../../solov2_utils/
+./docker/latest/start_devel.sh
+cd ../solov2_utils/
 docker cp camera_objects_msgs solo:/home/docker_solo/catkin_ws/src/
 docker cp camera_objects_visualizer solo:/home/docker_solo/catkin_ws/src/
 
