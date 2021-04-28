0469224332b452eeba16c34d03d9a16ea6f23a58
linukc <35877675+linukc@users.noreply.github.com>
2021-04-28 19:10:45 +0300

    Update fourth_terminal.bash

--- 3D_objects_detection/bash_scripts/fourth_terminal.bash
+++ 3D_objects_detection/bash_scripts/fourth_terminal.bash
@@ -3,4 +3,5 @@
 #файл должен находится в Repos/solov2_pytorch1.7/bash_scripts/ на роботе
 
 rosrun rviz rviz -d normal.rviz
+#(если не откроет искать normal rviz в Repos/solo_pytorch1.7/bash_scripts/
 
