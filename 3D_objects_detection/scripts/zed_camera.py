d8d0494e098759d76acbfc94f865f40436bcb777
linukc <35877675+linukc@users.noreply.github.com>
2021-05-26 16:50:28 +0300

    Update zed_camera.py

--- 3D_objects_detection/scripts/zed_camera.py
+++ 3D_objects_detection/scripts/zed_camera.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import os
 import rospy
 import numpy
 from message_filters import ApproximateTimeSynchronizer, Subscriber
@@ -13,6 +13,7 @@ from camera_objects_msgs.msg import ObjectArray, Object, RLE
 from cv_bridge import CvBridge
 from tf.transformations import quaternion_from_euler, unit_vector, projection_matrix, quaternion_from_matrix
 from visualization_msgs.msg import Marker
+from button_srv.srv import GetButtons, GetButtonsResponse
 
 bridge = CvBridge()
 
@@ -23,6 +24,7 @@ fy = 682.7612915039062
 c_x = 615.1388549804688
 c_y = 345.3089599609375
 
+pose_array = []
 
 def produce_center_point(roi):
     #simetric object
@@ -31,7 +33,8 @@ def produce_center_point(roi):
 def to_manipulator(pose):
     lc = pose.position
     lc_coord = numpy.array([[lc.x], [lc.y], [lc.z]])
-    T = [-0.2388, -0.06, 0.75]
+    #T = [-0.2388, -0.06, 0.75]
+    T = [-0.4088, 0.075, 0.76]
     R_cl2b = numpy.array([[0, 0, 1],
                           [-1, 0, 0],
                           [0, -1, 0]])
@@ -45,28 +48,6 @@ def get_z_with_neighborhoods(x ,y, depths, bbox):
     depths = depths[y-bbox.height/2:y+bbox.height/2, x-bbox.width/2:x+bbox.width/2]
     depths = [i for i in depths.flatten() if not numpy.isnan(i)]
     return round(float(numpy.median(depths)), 3)
-
-def plane_equation(pose1, pose2, pose3):
-    print('!!!')
-    print('a', pose1.position.x, pose1.position.y, pose1.position.z)
-    print('b', pose2.position.x, pose2.position.y, pose2.position.z)
-    print('c', pose3.position.x, pose3.position.y, pose3.position.z)
-
-    a1 = pose2.position.x - pose1.position.x
-    b1 = pose2.position.y - pose1.position.y
-    c1 = pose2.position.z - pose1.position.z
-    a2 = pose3.position.x - pose1.position.x
-    b2 = pose3.position.y - pose1.position.y
-    c2 = pose3.position.z - pose1.position.z
-
-    a = b1 * c2 - b2 * c1
-    b = a2 * c1 - a1 * c2
-    c = a1 * b2 - b1 * a2
-    d = (- a * pose1.position.x - b * pose1.position.y - c * pose1.position.z)
-
-    x = numpy.array([a, b, c])
-    a, b, c = x / numpy.linalg.norm(x)
-    return a, b, c
 
 def print_pose(pose):
     print pose.position.x, pose.position.y, pose.position.z
@@ -119,6 +100,7 @@ def make_surface(main_pose, pose_list):
 def callback(data_l, depth_l):
     if data_l.objects:
         bbox = data_l.objects[0].bbox
+        print("Number of objects-", len(data_l.objects))
         x, y = produce_center_point(bbox)
 
         try:
@@ -134,7 +116,7 @@ def callback(data_l, depth_l):
 
             #get points from button base surface in Base frame and in range 2x button size with the assumption 
             # that base surface is paralleled to the button
-            pose_r = to_manipulator(pose_in_camL_frame(int(x+1.5*bbox.width), y, depths, bbox))
+            pose_r = to_manipulator(pose_in_camL_frame(int(x+2*bbox.width), y, depths, bbox))
             pose_r.position.z = pose.position.z
             pose_r = pose_r - pose
             pose_r = pose_r.normalize()
@@ -179,23 +161,16 @@ def callback(data_l, depth_l):
             marker.points = [Point(0, 0, 0), pose_r.position,
                              Point(0, 0, 0), pose_n.position,
                              Point(0, 0, 0), pose_up.position]
-            marker_pub2.publish(marker)
+            #marker_pub2.publish(marker)
 
 
             #print(math.degrees(math.acos(a)), math.degrees(math.acos(b)), math.degrees(math.acos(c)))
             #quaternion to button
             #Q = quaternion_from_euler(math.acos(a),math.acos(b),math.acos(c), 'rxyz')
             #Q = quaternion_from_matrix(numpy.array([[pose_r.position.x, pose_r.position.y, pose_r.position.z, 0], 
-            #                                        [pose_n.position.x, pose_n.position.y, pose_n.position.z, 0], 
-            #                                        [pose_up.position.x, pose_up.position.y, pose_up.position.z, 0], 
-            #                                        [0, 0, 0, 1]]))
-    
-            #Q = quaternion_from_matrix(numpy.array([[0.71, 0.71, 0, 0], 
-            #                                        [-0.71, 0.71, 0, 0], 
-            #                                        [0, 0, 1, 0], 
-            #                                        [0, 0, 0, 1]]))
-            angel_x = int(math.degrees(math.acos(pose_r.position.x)))
-            print(angel_x)
+           
+            angel_x = int(math.degrees(math.acos(pose_r.position.x))) - 90
+            print("Angle", angel_x)
             Q = quaternion_from_matrix(numpy.array([[math.cos(math.radians(-angel_x)), -math.sin(math.radians(-angel_x)), 0, 0], 
                                                     [math.sin(math.radians(-angel_x)), math.cos(math.radians(-angel_x)), 0, 0], 
                                                     [0, 0, 1, 0], 
@@ -209,6 +184,12 @@ def callback(data_l, depth_l):
             pose_st.header.stamp = rospy.Time.now()
             pose_st.pose = pose
             pub.publish(pose_st)
+            #if len(pose_array) >= 10:
+            #    pose_array = []
+            if len(pose_array) >= 10:
+                global pose_array
+                pose_array = []
+            pose_array.append(pose)
             
         except Exception as e:
             print(e.message)
@@ -217,19 +198,32 @@ def callback(data_l, depth_l):
         print("not l objects")
 
 
+def handle_get_buttons(req):
+    d = {}
+    for pose in pose_array:
+        key = ";".join([str(round(pose.position.x,1)), str(round(pose.position.y,1)), str(round(pose.position.z, 1))])
+        if not key in d:
+            d[key] = []
+        d[key].append(pose)
+    poses = d[max(d.keys(), key=lambda key_: d[key_])]
+    global pose_array
+    pose_array = []
+    #os.system("rosnode kill /zed_node/left/solo_node_left")
+    return GetButtonsResponse(poses[0])
+
+
 if __name__ == '__main__':
     rospy.init_node('xyz')
 
     tss = ApproximateTimeSynchronizer([Subscriber('/zed_node/left/objects', ObjectArray), 
 				                       Subscriber('/zed_node/depth/depth_registered', Image)], 
-                                       queue_size=10, 
-                                       slop=3)
+                                       queue_size=30, 
+                                       slop=2)
     tss.registerCallback(callback)
-    
+    s = rospy.Service('get_buttons_pose', GetButtons, handle_get_buttons)
     pub = rospy.Publisher('/zed_node/button_Pose_from_Dsensor', PoseStamped, queue_size=10)
-    marker_pub = rospy.Publisher('/surface', Marker, queue_size=1)
-    marker_pub2 = rospy.Publisher('/normal', Marker, queue_size=1)
-    rate = rospy.Rate(3)
+    #marker_pub2 = rospy.Publisher('/normal', Marker, queue_size=1)
+    rate = rospy.Rate(6)
 
     while not rospy.is_shutdown():
         rate.sleep()
