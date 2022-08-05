25576b6ebc456088f78757e4d08a468f3d4b2dcf
Alexander Melekhin <melekhin.aa@mipt.ru>
2022-08-05 11:42:37 +0300

    feat: add reference_db volume mounting in docker start script

--- place_recognition/docker/start.sh
+++ place_recognition/docker/start.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+if [ $# != 1 ]
+then
+    echo "Usage:
+          bash start.sh [DATABASE_DIR]
+         "
+    exit 1
+fi
+
+get_real_path(){
+  if [ "${1:0:1}" == "/" ]; then
+    echo "$1"
+  else
+    realpath -m "$PWD"/"$1"
+  fi
+}
+
+DATABASE_DIR=$(get_real_path "$1")
+echo "Reference database directory: "$DATABASE_DIR
+
+if [ ! -d $DATABASE_DIR ]
+then
+    echo "ERROR: DATABASE_DIR=$DATABASE_DIR is not an existing directory."
+exit 1
+fi
+
 orange=`tput setaf 3`
 reset_color=`tput sgr0`
 
@@ -32,5 +57,6 @@ xhost +
         --net host \
         -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
         -v `pwd`/../place_recognition/:/home/docker_place_recognition/catkin_ws/src/place_recognition:rw \
+        -v $DATABASE_DIR/:/home/docker_place_recognition/reference_db:rw \
         ${ARCH}noetic/strl_place_recognition:latest
 xhost -
