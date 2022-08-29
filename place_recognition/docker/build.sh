8b29501dd5f14bc5e3f2a98bc8d4d93484e1b5de
Alexander Melekhin <melekhin.aa@mipt.ru>
2022-08-29 19:55:07 +0300

    fix: optimized docker build script

--- place_recognition/docker/build.sh
+++ place_recognition/docker/build.sh
@@ -4,18 +4,21 @@ orange=`tput setaf 3`
 reset_color=`tput sgr0`
 
 ARCH=`uname -m`
-NUM_THREADS=`nproc`
+
+if [ $ARCH != "x86_64" ]
+then
+    echo "Architecture ${orange}${ARCH}${reset_color} is not supported"
+    exit 1
+fi
 
 cd "$(dirname "$0")"
 root_dir=$PWD 
 cd $root_dir/..
 
 echo "Building for ${orange}${ARCH}${reset_color}"
-echo "Building is carried out in ${orange} ${NUM_THREADS} threads${reset_color}"
 
 docker build . \
     -f docker/Dockerfile.${ARCH} \
     --build-arg UID=$(id -u) \
     --build-arg GID=$(id -g) \
-    --build-arg NUM_THREADS=${NUM_THREADS} \
     -t ${ARCH}noetic/strl_place_recognition:latest
